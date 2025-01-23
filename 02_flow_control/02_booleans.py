9288de4d9672545bae979251d7fb9a1b8351eec5
Franklin B <alejobola16@gmail.com>
2025-01-22 23:14:27 -0500

    First Changes

--- 02_flow_control/02_booleans.py
+++ 02_flow_control/02_booleans.py
@@ -15,7 +15,7 @@ print(False)
 # Operadores de comparación: devuelven un valor booleano.
 print("\nOperadores de comparación:")
 print("5 > 3:", 5 > 3)        # True
-print("5 < 3:", 5 < 3)        # False
+print("5 < 3:", 5 < 3)   # False
 print("5 == 5:", 5 == 5)      # True (igualdad)
 print("5 != 3:", 5 != 3)      # True (desigualdad)
 print("5 >= 5:", 5 >= 5)      # True (mayor o igual que)
