f9f5d35970026634299924a249622d8893048371
2G2-99 <79936466+2G2-99@users.noreply.github.com>
2025-02-04 10:01:19 +0000

    Segunda leccion completada

--- 02_flow_control/04_list_methods.py
+++ 02_flow_control/04_list_methods.py
@@ -74,34 +74,56 @@ print('🐹' in animals) # -> False
 # EJERCICOS
 # Usa siempre que puedas los métodos que has aprendido
 ###
+print("\n//////")
 
-# Ejercicio 1: Añadir y modificar elementos
+print("\n  Ejercicio 1: Añadir y modificar elementos")
 # Crea una lista con los números del 1 al 5.
 # Añade el número 6 al final usando append().
 # Inserta el número 10 en la posición 2 usando insert().
 # Modifica el primer elemento de la lista para que sea 0.
+lista = [1, 2, 3, 4, 5]
+lista.append(6)
+lista.insert(2, 10)
+lista[0] = 0
+print(lista)
 
-# Ejercicio 2: Combinar y limpiar listas
+print("\n  Ejercicio 2: Combinar y limpiar listas")
 # Crea dos listas:
-# lista_a = [1, 2, 3]
-# lista_b = [4, 5, 6, 1, 2]
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6, 1, 2]
 # Extiende lista_a con lista_b usando extend().
 # Elimina la primera aparición del número 1 en lista_a usando remove().
 # Elimina el elemento en el índice 3 de lista_a usando pop(). Imprime el elemento eliminado.
 # Limpia completamente lista_b usando clear().
 
-# Ejercicio 3: Slicing y eliminación con del
+lista_a.extend(lista_b)
+lista_a.remove(1)
+print(lista_a)
+print(lista_a.pop(3))
+lista_b.clear()
+
+print("\n  Ejercicio 3: Slicing y eliminación con del")
 # Crea una lista con los números del 1 al 10.
 # Utiliza slicing y del para eliminar los elementos desde el índice 2 hasta el 5 (sin incluir el 5).
 # Imprime la lista resultante.
+lista = list(range(1, 11))
+print(lista)
+del lista[2:5]
+print(lista)
 
-# Ejercicio 4: Ordenar y contar
+print("\n  Ejercicio 4: Ordenar y contar")
 # Crea una lista con los siguientes números: [5, 2, 8, 1, 9, 4, 2].
 # Ordena la lista de forma ascendente usando sort().
 # Cuenta cuántas veces aparece el número 2 en la lista usando count().
 # Comprueba si el número 7 está en la lista usando in.
 
-# Ejercicio 5: Copia vs. Referencia
+lista = [5, 2, 8, 1, 9, 4, 2]
+lista.sort()
+lista.count(2)
+print(lista)
+print(7 in lista)
+
+print("\n  Ejercicio 5: Copia vs. Referencia")
 # Crea una lista llamada original con los números [1, 2, 3].
 # Crea una copia de la lista original llamada copia_1 usando slicing.
 # Crea otra copia llamada copia_2 usando copy().
@@ -109,6 +131,17 @@ print('🐹' in animals) # -> False
 # Modifica el primer elemento de la lista referencia a 10.
 # Imprime las cuatro listas (original, copia_1, copia_2, referencia) y observa los cambios.
 
-# Ejercicio 6: Ordenar strings sin diferenciar mayúsculas y minúsculas.
+original = [1, 2 , 3]
+copia_1 = original[0:]
+copia_2 = original.copy()
+referencia = original
+referencia[0] = 10
+print(f"Original: {original}", f"Copia 1: {copia_1}", f"Copia 2: {copia_2}", f"Referencia: {referencia}", sep="\n")
+
+print("\n  Ejercicio 6: Ordenar strings sin diferenciar mayúsculas y minúsculas.")
 # Crea una lista con las siguientes cadenas: ["Manzana", "pera", "BANANA", "naranja"].
 # Ordena la lista sin diferenciar entre mayúsculas y minúsculas.
+
+lista_de_cadenas = ["Manzana", "pera", "BANANA", "naranja"]
+lista_de_cadenas.sort(key=str.lower)
+print(lista_de_cadenas)
